126a8e3e45b57291671fabd1003fa5963b547988
TafitasoaNooms27 <rrnomenatafitasoa@gmail.com>
2023-06-22 00:10:21 +0300

    MAJ resaka AWS EC2 CONFIGS

--- gestion_affectation/app/admin.py
+++ gestion_affectation/app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from app.models import Employe, Lieu , Affecter , AuditEmp , AuditLieu , AuditAffecter
+
 # Register your models here.
+admin.site.register(Employe)
+admin.site.register(Lieu)
+admin.site.register(Affecter)
+admin.site.register(AuditEmp)
+admin.site.register(AuditLieu)
+admin.site.register(AuditAffecter)
